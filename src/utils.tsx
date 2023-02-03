32a9bae616a8785592fa99e4bd8ad1ffedbecc30
Hoang Trong Tue <tueht@users.noreply.github.com>
2023-02-03 13:41:29 +0700

    Update utils.tsx
    fix for conditional page renderring

--- src/utils.tsx
+++ src/utils.tsx
@@ -3,6 +3,9 @@ import { StyleSheet, View } from 'react-native';
 
 export const childrenWithOverriddenStyle = (children?: ReactNode) => {
   return Children.map(children, (child) => {
+    if (!child) {
+      return null;
+    }
     const element = child as React.ReactElement;
     return (
       // Add a wrapper to ensure layout is calculated correctly
